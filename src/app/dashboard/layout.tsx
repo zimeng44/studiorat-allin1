fe8e9e29a8f2b855b94158927d58660418db12c5
zi_meng <zimeng44@gmail.com>
2024-07-31 04:46:20 -0400

    navigation menu bugs fixed

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -44,13 +44,17 @@ export default async function DashboardLayout({
                 Account
               </Link>
 
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="/dashboard/booking"
-              >
-                <BookPlus className="h-4 w-4" />
-                Booking
-              </Link>
+              {currentRole === "Admin" || currentRole === "Monitor" ? (
+                <Link
+                  className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
+                  href="/dashboard/booking"
+                >
+                  <BookPlus className="h-4 w-4" />
+                  Booking
+                </Link>
+              ) : (
+                ``
+              )}
 
               {currentRole === "Admin" || currentRole === "Monitor" ? (
                 <Link
