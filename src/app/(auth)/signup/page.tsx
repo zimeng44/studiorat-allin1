5a5b866b3eaac8d0e6a7e486699c8c3f2e2508e7
zi_meng <zimeng44@gmail.com>
2024-08-01 22:38:45 -0400

    signup bugs fixed

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -2,7 +2,10 @@ import { SignupForm } from "@/components/forms/SignupForm";
 import { getUserMeLoader } from "@/data/services/get-user-me-loader";
 
 export default async function SignUpRoute() {
-  const { data: currentUser } = await getUserMeLoader();
+  const { ok: ok, data: currentUser } = await getUserMeLoader();
+
+  if (!ok)
+    return <p>Only Admin and Monitor Allowed to sign up for new user.</p>;
 
   if (currentUser.role.name !== "Admin" && currentUser.role.name !== "Monitor")
     return <p>Only Admin and Monitor Allowed to sign up for new user.</p>;
