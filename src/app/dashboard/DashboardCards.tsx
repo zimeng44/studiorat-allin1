280fe62872379bc68e45969af9d6020da1473297
zi_meng <zimeng44@gmail.com>
2024-08-04 04:53:16 -0400

    bugs fixed

--- src/app/dashboard/DashboardCards.tsx
+++ src/app/dashboard/DashboardCards.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import {
-  Activity,
   ArrowUpRight,
   Barcode,
   BookPlus,
-  CircleUser,
   ClipboardPlus,
-  CreditCard,
-  DollarSign,
-  Menu,
-  Package2,
-  Search,
-  Users,
   Vault,
 } from "lucide-react";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import {
@@ -26,16 +16,6 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-// import {
-//   DropdownMenu,
-//   DropdownMenuContent,
-//   DropdownMenuItem,
-//   DropdownMenuLabel,
-//   DropdownMenuSeparator,
-//   DropdownMenuTrigger,
-// } from "@/components/ui/dropdown-menu";
-// import { Input } from "@/components/ui/input";
-// import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import {
   Table,
   TableBody,
